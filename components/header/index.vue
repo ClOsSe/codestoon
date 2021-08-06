3038746739ced2f9ff0f48ceff2f969b76576ef0
mohammad goudarzi <webmaster.webdeveloper@gmail.com>
2021-08-06 02:53:50 -0700

    init header stracture

--- components/header/index.vue
+++ components/header/index.vue
@@ -1,5 +1,21 @@
 <template>
-    <h1>
-        header
-    </h1>
+    <div>
+        <div class="md-xsmal-hide">
+            <Desktop />
+        </div>
+        <div class="md-xlarge-hide md-large-hide">
+            <Mobile />
+        </div>
+
+    </div>
 </template>
+<script >
+import Mobile from "./mobile"
+import Desktop from "./desktop"
+export default {
+    components:{
+        Mobile,
+        Desktop
+    }
+}
+</script>
