93a0c5bfaf0bfa05279d031dcbaca667908728bf
mohammad goudarzi <webmaster.webdeveloper@gmail.com>
2021-08-12 00:43:04 -0700

    title file created

---
+++ components/title/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="row col-12 center title">
+        <h2>{{Title}}</h2>
+    </div>
+</template>
+<script>
+export default {
+    props:['Title'],
+    mounted(){
+        console.log(this.Title);
+    }
+}
+</script>
+<style lang="scss" scoped>
+.title{
+    padding: 20px;
+    margin: 25px;
+    margin-top: 35px;
+    font-size: 24px;
+    font-family: vazir_bold;
+}
+</style>
