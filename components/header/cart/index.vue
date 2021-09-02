2ab1715e751b1fe495f9737dd3517f2dd7ff483d
mohammad goudarzi <webmaster.webdeveloper@gmail.com>
2021-09-01 23:52:19 -0700

    create cart file

---
+++ components/header/cart/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+     <cart class="cart" />
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.cart{
+        color:wheat;
+}
+</style>
