c29cd39a416c29459df1b36959a01f9ea4f99c2c
mohammad goudarzi <webmaster.webdeveloper@gmail.com>
2021-09-01 23:42:17 -0700

    install vue-material-design-icons

---
+++ plugins/icons/materialDesignIcons.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import MenuIcon from 'vue-material-design-icons/Menu.vue';
+import Whatsapp from 'vue-material-design-icons/Whatsapp.vue';
+import Telegram from 'vue-material-design-icons/SendCircleOutline.vue';
+import Instagram from 'vue-material-design-icons/Instagram.vue';
+Vue.component('menu-icon', MenuIcon);
+Vue.component('whatsapp', Whatsapp);
+Vue.component('telegram', Telegram);
+Vue.component('instagram', Instagram);
